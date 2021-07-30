f01e1a0125490e854536721cceebfffd57834355
DeasyNabila <deasy_nabila_29rpl@student.smktelkom-mlg.sch.id>
2021-07-30 16:26:31 +0700

    UKL 2

---
+++ src/latsol2/Latsol2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latsol2;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class Latsol2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int a = 0; a < 5; a++){
+            for(int i=0; i<a; i++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
